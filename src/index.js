776f8a4b0ed01302d0cac57191f1fba38647e934
ilkhomjon2002 <dergasbaev@gmail.com>
2024-02-21 14:15:15 +0900

    every thing is readdy

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
+// "9nimxmk4FIIAgHKQ"  supabase this project`s password
